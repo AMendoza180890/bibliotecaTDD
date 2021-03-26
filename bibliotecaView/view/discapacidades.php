86da633db4cccc87ccd45c4792551f7a24d56f56
AMendoza180890 <allanmaleman@gmail.com>
2021-03-25 18:02:46 -0600

    Se modifico plantilla, se agregaron paginas habilidades, espiritual, discapacidades, catlectura y autismo y academico

--- bibliotecaView/view/discapacidades.php
+++ bibliotecaView/view/discapacidades.php
@@ -1,27 +1,48 @@
 <div class="container">
     <div class="page-header">
-        <h1 class="all-tittles">Sistema bibliotecario <small>Inicio</small></h1>
+        <h1 class="all-tittles">Recursos sobre discapacidades</h1>
     </div>
 </div>
 <section class="full-reset text-center" style="padding: 40px 0;">
-    <a href="catlectura">
+    <a href="autismo">
         <article class="tile">
             <div class="tile-icon full-reset"><i class="zmdi zmdi-book"></i></div>
-            <div class="tile-name all-tittles">Lectura</div>
+            <div class="tile-name all-tittles">Autismo</div>
             <div class="tile-num full-reset"></div>
         </article>
     </a>
-    <a href="catmat">
+    <a href="emocional">
         <article class="tile">
             <div class="tile-icon full-reset"><i class="zmdi zmdi-dialpad"></i></div>
-            <div class="tile-name all-tittles">Matemática</div>
+            <div class="tile-name all-tittles">Emocional</div>
             <div class="tile-num full-reset"></div>
         </article>
     </a>
-    <a href="catadecuaciones">
+    <a href="intelectual">
         <article class="tile">
             <div class="tile-icon full-reset"><i class="zmdi zmdi-assignment-o"></i></div>
-            <div class="tile-name all-tittles">Adecuaciones/Modificaciones</div>
+            <div class="tile-name all-tittles">Intelectual</div>
+            <div class="tile-num full-reset"></div>
+        </article>
+    </a>
+    <a href="lenguaje">
+        <article class="tile">
+            <div class="tile-icon full-reset"><i class="zmdi zmdi-assignment-o"></i></div>
+            <div class="tile-name all-tittles">Lenguaje/Comunicación</div>
+            <div class="tile-num full-reset"></div>
+        </article>
+    </a>
+    <a href="psicomotor">
+        <article class="tile">
+            <div class="tile-icon full-reset"><i class="zmdi zmdi-assignment-o"></i></div>
+            <div class="tile-name all-tittles">Psicomotor</div>
+            <div class="tile-num full-reset"></div>
+        </article>
+    </a>
+    <a href="aprendizaje">
+        <article class="tile">
+            <div class="tile-icon full-reset"><i class="zmdi zmdi-assignment-o"></i></div>
+            <div class="tile-name all-tittles">Trastorno de Aprendizaje</div>
             <div class="tile-num full-reset"></div>
         </article>
     </a>
--- bibliotecaView/view/menu.php
+++ bibliotecaView/view/menu.php
@@ -1,32 +1,33 @@
 <div class="navbar-lateral full-reset">
-        <div class="visible-xs font-movile-menu mobile-menu-button"></div>
-        <div class="full-reset container-menu-movile custom-scroll-containers">
-            <div class="logo full-reset all-tittles">
-                <i class="visible-xs zmdi zmdi-close pull-left mobile-menu-button" style="line-height: 55px; cursor: pointer; padding: 0 10px; margin-left: 7px;"></i> 
-                sistema bibliotecario
-            </div>
-            <div class="full-reset" style="background-color:#2B3D51; padding: 10px 0; color:#fff;">
-                <figure>
-                    <img src="bibliotecaView/assets/img/logo.png" alt="Biblioteca" class="img-responsive center-box" style="width:15%;">
-                </figure>
-                <p class="text-center" style="padding-top: 15px;">Sistema Bibliotecario</p>
-            </div>
-            <div class="full-reset nav-lateral-list-menu">
-                <ul class="list-unstyled">
-                    <li><a href="home"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
-                    <li><a href="academico"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Académico</a></li>
-                    <li><a href="autismo"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Autismo</a></li>
-                    <li><a href="conducta"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Conducta</a></li>
-                    <li><a href="curriuculum"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Currículo Tesoros 2018</a></li>
-                    <li><a href="curso"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Curso Dra. VanTol Iowa</a></li>
-                    <li><a href="ingles"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; En Ingles</a></li>
-                    <li><a href="estimulacion"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Estimulacion Temprana</a></li>
-                    <li><a href="fisioterapia"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Fisioterapia</a></li>
-                    <li><a href="historia"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Historias Sociales</a></li>
-                    <li><a href="iglesias"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Iglesias</a></li>
-                    <li><a href="lenguaje"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Lenguaje</a></li>
-                    <li><a href="piedu"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Plan Individualizado de Educación</a></li>
-                    <!-- <li>
+    <div class="visible-xs font-movile-menu mobile-menu-button"></div>
+    <div class="full-reset container-menu-movile custom-scroll-containers">
+        <div class="logo full-reset all-tittles">
+            <i class="visible-xs zmdi zmdi-close pull-left mobile-menu-button" style="line-height: 55px; cursor: pointer; padding: 0 10px; margin-left: 7px;"></i>
+            sistema bibliotecario
+        </div>
+        <div class="full-reset" style="background-color:#2B3D51; padding: 10px 0; color:#fff;">
+            <figure>
+                <img src="bibliotecaView/assets/img/logo.png" alt="Biblioteca" class="img-responsive center-box" style="width:15%;">
+            </figure>
+            <p class="text-center" style="padding-top: 15px;">Sistema Bibliotecario</p>
+        </div>
+        <div class="full-reset nav-lateral-list-menu">
+            <ul class="list-unstyled">
+                <li><a href="home"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
+                <li><a href="academico"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Académico</a></li>
+                <li><a href="conducta"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Conducta</a></li>
+                <li><a href="curriuculum"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Currículo Tesoros 2018</a></li>
+                <li><a href="discapacidades"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Discapacidades</a></li>
+                <li><a href="ingles"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; En Inglés</a></li>
+                <li><a href="estimulacion"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Estimulacion Temprana</a></li>
+                <li><a href="fisioterapia"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Fisioterapia</a></li>
+                <li><a href="historia"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Habilidades Sociales</a></li>
+                <li><a href="iglesias"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Iglesias</a></li>
+                <li><a href="lenguaje"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Logopedia</a></li>
+                <li><a href="piedu"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Plan Individualizado</a></li>
+                <li><a href="espiritual"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Recursos Espirituales</a></li>
+                <li><a href="habilidad"><i class="zmdi zmdi-book zmdi-hc-fw"></i>&nbsp;&nbsp; Habilidades de la Vida Diaria</a></li>
+                <!-- <li>
                         <div class="dropdown-menu-button"><i class="zmdi zmdi-case zmdi-hc-fw"></i>&nbsp;&nbsp; Administración <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
                         <ul class="list-unstyled">
                             <li><a href="institution.html"><i class="zmdi zmdi-balance zmdi-hc-fw"></i>&nbsp;&nbsp; Datos institución</a></li>
@@ -44,7 +45,7 @@
                             <li><a href="personal.html"><i class="zmdi zmdi-male-female zmdi-hc-fw"></i>&nbsp;&nbsp; Nuevo personal administrativo</a></li>
                         </ul>
                     </li> -->
-                </ul>
-            </div>
+            </ul>
         </div>
     </div>
+</div>
