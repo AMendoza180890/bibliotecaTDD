8f37634023caedae6adf9430937927f891278950
AMendoza180890 <allanmaleman@gmail.com>
2021-03-28 19:40:05 -0600

    Se trabajó en todo la parte de visitante y se publicó

--- bibliotecaView/view/historia.php
+++ bibliotecaView/view/historia.php
@@ -1,10 +1,10 @@
 <div class="container-fluid">
                         <div class="page-header">
-                          <h2 class="all-tittles">Recurso Academico</h2>
+                          <h2 class="all-tittles">Recurso de Historias Sociales</h2>
                         </div>
                         <div class="row">
                             <div class="col-xs-12">
-                                <h3 class="text-center all-tittles">Todos los recursos para el area academica</h3>
+                                <h3 class="text-center all-tittles">Todos los recursos de Historias Sociales</h3>
                                 <div class="table-responsive">
                                     <table class="table table-hover text-center">
                                         <thead>
