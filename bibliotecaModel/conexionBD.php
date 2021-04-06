2df92ef9425ee8e2cb92864e23e89f5d5e0ea2a6
TDDTecnologia_Allan <allanmaleman@gmail.com>
2021-04-06 14:19:17 -0600

    Se modifico el archivo de conexion a la base de datos para trabajar en local

--- bibliotecaModel/conexionBD.php
+++ bibliotecaModel/conexionBD.php
@@ -2,7 +2,9 @@
 class conexionBD{
     public static function conexion(){
         try {
-            $bd = new PDO("mysql:host=localhost;dbname=tesorosd_bdbibliotecatdd","tesorosd_library","yOLtH_qK?8gt");
+            $bd = new PDO("mysql:host=localhost;dbname=bdbibliotecatdd","root","");
+
+            /*$bd = new PDO("mysql:host=localhost;dbname=tesorosd_bdbibliotecatdd","tesorosd_library","yOLtH_qK?8gt");*/
 
             $bd -> exec("set names utf8");
 
