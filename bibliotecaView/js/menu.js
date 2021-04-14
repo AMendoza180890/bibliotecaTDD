330bc746855e9ce3bc99a98e0a0c87882f4881db
TDDTecnologia_Allan <allanmaleman@gmail.com>
2021-04-14 14:26:31 -0600

    El boton de buscar esta funcional en 2 partes del sistema y cargan bien, ya se publico esta solucion

--- bibliotecaView/js/menu.js
+++ bibliotecaView/js/menu.js
@@ -1,4 +1,8 @@
-$("#menuOpciones li a").on("click", function () {
-  $("li a.activo").removeClass("activo");
-  $(this).addClass("activo");
+// $("#menuOpciones li a").on("click", function () {
+//   $("li a.activo").removeClass("activo");
+//   $(this).addClass("activo");
+// });
+
+document.getElementById("btn-search").addEventListener("click", function () {
+  window.location = "buscar";
 });
--- bibliotecaView/view/menuSuperior.php
+++ bibliotecaView/view/menuSuperior.php
@@ -20,6 +20,9 @@
                  <li  class="tooltips-general btn-help" data-placement="bottom" title="Ayuda">
                      <i class="zmdi zmdi-help-outline zmdi-hc-fw"></i>
                  </li>
+                 <li  class="tooltips-general" data-placement="bottom" title="Buscar">
+                     <i class="zmdi zmdi-search zmdi-hc-fw" id="btn-search"></i>
+                 </li>
                  <li class="mobile-menu-button visible-xs" style="float: left !important;">
                      <i class="zmdi zmdi-menu"></i>
                  </li>
