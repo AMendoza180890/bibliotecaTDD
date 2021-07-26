691e548291a4b4e4d464fa82e2fff5a1eebb7acb
AMendoza180890 <allanmaleman@gmail.com>
2021-07-26 11:59:33 -0600

    Se restablecio la cadena de conexion

---
+++ bibliotecaModel/envConexion.php
@@ -0,0 +1,18 @@
+<?php
+    // en arreglo con clave valor
+    // $parametrosConexion = [
+    //     'bd' => "bdbibliotecatdd",
+    //     'server' => "localhost",
+    //     'user' => "root",
+    //     'passw' => "",
+    // ];
+
+    // en variables
+      $databaseName = "tesorosd_bdbibliotecatdd";
+      $servidor = "localhost";
+      $user = "tesorosd_library";
+      $passw = "yOLtH_qK?8gt";
+
+     //include '../bibliotecaModel/conexionBD.php';
+
+?>
