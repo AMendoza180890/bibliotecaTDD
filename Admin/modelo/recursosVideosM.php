8dbb346e9696e548baa550b662c493c4e988b286
TDDTecnologia_Allan <allanmaleman@gmail.com>
2021-07-30 15:12:01 -0600

    Se ajusta controlador y modelo de recursosVideo

--- Admin/modelo/recursosVideosM.php
+++ Admin/modelo/recursosVideosM.php
@@ -1,103 +1,100 @@
 <?php
 require_once 'conexionBD.php';
-// class recursoM extends conexionBD{
-//     public static function mostrarRecursoM(){
-//         try {
-//             $pdo = conexionBD::conexion()->prepare("SELECT id, nombre, ruta, titulo, descripcion, autor FROM  catrecursos");
-//             $pdo ->execute();
-//             return $pdo->fetchAll();
+class recursoVideosM extends conexionBD{
+    
+    public static function mostrarRecursoVideoM(){
+        try {
+            $pdo = conexionBD::conexion()->prepare("SELECT id, nombre, link, titulo, descripcion, autor FROM catrecursosvideos");
+            $pdo ->execute();
+            return $pdo->fetchAll();
 
-//         } catch (exception $ex) {
-//             echo 'Error - '.$ex;
-//         }
-//     }
+        } catch (exception $ex) {
+            echo 'Error - '.$ex;
+        }
+    }
 
-//     public static function registrarRecursoM($datosRecurso){
-//         try {
-//             $pdo = conexionBD::conexion()->prepare("INSERT INTO catrecursos (nombre, ruta, titulo, descripcion, autor, tipo, resumen) VALUES (:nombre, :ruta, :titulo, :descripcion, :autor, :tipo, :resumen)");
-//             $pdo    ->  bindParam("nombre",$datosRecurso["nombreArchivo"],PDO::PARAM_STR);
-//             $pdo    ->  bindParam("ruta", $datosRecurso["ruta"],PDO::PARAM_STR);
-//             $pdo    ->  bindParam("titulo", $datosRecurso["titulo"], PDO::PARAM_STR);
-//             $pdo    ->  bindParam("descripcion", $datosRecurso["detalle"], PDO::PARAM_STR);
-//             $pdo    ->  bindParam("autor", $datosRecurso["autor"], PDO::PARAM_STR);
-//             $pdo    ->  bindParam("tipo", $datosRecurso["tipoArchivo"], PDO::PARAM_STR);
-//             $pdo    ->  bindParam("resumen", $datosRecurso["resumenN"], PDO::PARAM_STR);
+    public static function registrarRecursoVideoM($datosRecurso){
+        try {
+            $pdo = conexionBD::conexion()->prepare("INSERT INTO catrecursosvideos(nombre, link, titulo, descripcion, autor) VALUES (:nombre, :link, :titulo, :descripcion, :autor)");
+            $pdo    ->  bindParam("nombre",$datosRecurso["nombreArchivo"],PDO::PARAM_STR);
+            $pdo    ->  bindParam("link", $datosRecurso["link"],PDO::PARAM_STR);
+            $pdo    ->  bindParam("titulo", $datosRecurso["titulo"], PDO::PARAM_STR);
+            $pdo    ->  bindParam("descripcion", $datosRecurso["detalle"], PDO::PARAM_STR);
+            $pdo    ->  bindParam("autor", $datosRecurso["autor"], PDO::PARAM_STR);
 
-//             if($pdo -> execute()){
-//                 return true;
-//             }else {
-//                 return false;
-//             }
+            if($pdo -> execute()){
+                return true;
+            }else {
+                return false;
+            }
 
-//         } catch (exception $ex) {
-//             echo 'Error - '.$ex;
-//         }
-//     }
+        } catch (exception $ex) {
+            echo 'Error - '.$ex;
+        }
+    }
 
-//     public static function obtenerRecursoRegistradoM($codigoRecurso){
-//         try {
-//             $pdo = conexionBD::conexion()->prepare("SELECT id, nombre, ruta, titulo, descripcion, autor, tipo, resumen FROM  catrecursos WHERE id = :id");
-//             $pdo -> bindParam("id",$codigoRecurso,PDO::PARAM_INT);
-//             $pdo -> execute();
-//             return $pdo->fetch();
-//         } catch (exception $ex) {
-//             echo 'Error - '.$ex;
-//         }
-//     }
+    public static function obtenerRecursoRegistradoVideoM($codigoRecurso){
+        try {
+            $pdo = conexionBD::conexion()->prepare("SELECT id, nombre, link, titulo, descripcion, autor FROM catrecursosvideos WHERE id = :id");
+            $pdo -> bindParam("id",$codigoRecurso,PDO::PARAM_INT);
+            $pdo -> execute();
+            return $pdo->fetch();
+        } catch (exception $ex) {
+            echo 'Error - '.$ex;
+        }
+    }
 
-//     public static function actualizarRecursoM($datosRecursoActualizado){
-//         try {
-//             $pdo = conexionBD::conexion()->prepare("UPDATE catrecursos SET nombre = :nombre, ruta = :ruta, titulo = :titulo, descripcion = :descripcion, autor=:autor, tipo=:tipo, resumen=:resumen WHERE id = :id");
-//             $pdo -> bindParam("id",$datosRecursoActualizado["id"],PDO::PARAM_INT);
-//             $pdo -> bindParam("nombre",$datosRecursoActualizado["nombre"],PDO::PARAM_STR);
-//             $pdo -> bindParam("ruta",$datosRecursoActualizado["ruta"],PDO::PARAM_STR);
-//             $pdo -> bindParam("titulo",$datosRecursoActualizado["titulo"],PDO::PARAM_STR);
-//             $pdo -> bindParam("descripcion",$datosRecursoActualizado["detalle"],PDO::PARAM_STR);
-//             $pdo -> bindParam("autor",$datosRecursoActualizado["autor"],PDO::PARAM_STR);
-//             $pdo -> bindParam("tipo",$datosRecursoActualizado["tipoArchivoEditado"],PDO::PARAM_STR);
-//             $pdo -> bindParam("resumen",$datosRecursoActualizado["resumenEditado"],PDO::PARAM_STR);
+    public static function actualizarRecursoVideoM($datosRecursoActualizado){
+        try {
+            $pdo = conexionBD::conexion()->prepare("UPDATE catrecursosvideos SET nombre=:nombre,link=:link,titulo=titulo,descripcion=:descripcion,autor=:autor WHERE id=:id");
+            $pdo -> bindParam("id",$datosRecursoActualizado["id"],PDO::PARAM_INT);
+            $pdo -> bindParam("nombre",$datosRecursoActualizado["nombre"],PDO::PARAM_STR);
+            $pdo -> bindParam("link",$datosRecursoActualizado["link"],PDO::PARAM_STR);
+            $pdo -> bindParam("titulo",$datosRecursoActualizado["titulo"],PDO::PARAM_STR);
+            $pdo -> bindParam("descripcion",$datosRecursoActualizado["detalle"],PDO::PARAM_STR);
+            $pdo -> bindParam("autor",$datosRecursoActualizado["autor"],PDO::PARAM_STR);
 
-//             if ($pdo->execute()) {
-//                 return true;
-//             }else{
-//                 return false;
-//             }
-//         } catch (exception $ex) {
-//             echo 'Error - '.$ex;
-//         }
-//     }
+            if ($pdo->execute()) {
+                return true;
+            }else{
+                return false;
+            }
+        } catch (exception $ex) {
+            echo 'Error - '.$ex;
+        }
+    }
 
-//     public  function asignarEtiquetas($etiquetasAsignadas,$id){
-//         try {
-//             $pdo = conexionBD::conexion()->prepare("INSERT INTO unionetiquetascatrecurso( idRecurso, idEtiqueta) VALUES (:idRecurso, :idEtiqueta)");
-//             $pdo ->bindParam("idRecurso",$id,PDO::PARAM_INT);
-//             $pdo ->bindParam("idEtiqueta",$etiquetasAsignadas,PDO::PARAM_INT);
-//             $pdo->execute();
+    public  function asignarEtiquetasVideos($etiquetasAsignadas,$id){
+        try {
+            $pdo = conexionBD::conexion()->prepare("INSERT INTO unionetiquetacatrecursovideo(recursovideoid, etiquetaid) VALUES (:idRecurso, :idEtiqueta)");
+            $pdo ->bindParam("idRecurso",$id,PDO::PARAM_INT);
+            $pdo ->bindParam("idEtiqueta",$etiquetasAsignadas,PDO::PARAM_INT);
+            $pdo->execute();
 
-//         } catch (exception $ex) {
-//             echo 'Error -'.$ex;
-//         }
-//     }
+        } catch (exception $ex) {
+            echo 'Error -'.$ex;
+        }
+    }
 
-//     public function eliminarEtiquetas($id){
-//         try {
-//             $pdo = conexionBD::conexion()->prepare("DELETE FROM unionetiquetascatrecurso WHERE idRecurso = :id");
-//             $pdo ->bindParam("id",$id,PDO::PARAM_INT);
-//             $pdo->execute();
-//         } catch (exception $ex) {
-//             echo 'Error -'.$ex;
-//         }
-//     }
+    public function eliminarEtiquetasVideos($id){
+        try {
+            $pdo = conexionBD::conexion()->prepare("DELETE FROM unionetiquetacatrecursovideo WHERE recursovideoid = :id");
+            $pdo ->bindParam("id",$id,PDO::PARAM_INT);
+            $pdo->execute();
+        } catch (exception $ex) {
+            echo 'Error -'.$ex;
+        }
+    }
 
-//     public static function obtenerEtiquetaRecurso($codigoRecurso){
-//         try {
-//             $pdo = conexionBD::conexion()->prepare("SELECT catrecursos.id, catetiquetas.id AS etiquetaId, catetiquetas.etiquetaDescripcion FROM catetiquetas INNER JOIN unionetiquetascatrecurso on catetiquetas.id = unionetiquetascatrecurso.idEtiqueta INNER JOIN catrecursos on unionetiquetascatrecurso.idRecurso = catrecursos.id where catrecursos.id = :id");
-//             $pdo ->bindParam("id",$codigoRecurso,PDO::PARAM_INT);
-//             $pdo ->execute();
-//             return $pdo->fetchAll();
-//         } catch (exception $ex) {
-//             echo 'Error - '.$ex;
-//         }
-//     }
-// }
+    // public static function obtenerEtiquetaRecursoVideo($codigoRecurso){
+    //     try {
+    //         $pdo = conexionBD::conexion()->prepare("SELECT catrecursos.id, catetiquetas.id AS etiquetaId, catetiquetas.etiquetaDescripcion FROM catetiquetas INNER JOIN unionetiquetascatrecurso on catetiquetas.id = unionetiquetascatrecurso.idEtiqueta INNER JOIN catrecursos on unionetiquetascatrecurso.idRecurso = catrecursos.id where catrecursos.id = :id");
+    //         $pdo ->bindParam("id",$codigoRecurso,PDO::PARAM_INT);
+    //         $pdo ->execute();
+    //         return $pdo->fetchAll();
+    //     } catch (exception $ex) {
+    //         echo 'Error - '.$ex;
+    //     }
+    // }
+}
 ?>
