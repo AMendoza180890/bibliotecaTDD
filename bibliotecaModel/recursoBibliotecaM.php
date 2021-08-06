3359d9f3d863ad33f5ea7685b53023b7a16005d7
TDDTecnologia_Allan <allanmaleman@gmail.com>
2021-08-06 14:19:46 -0600

    Se agrego DISTINCT a la consulta que retorna registros de recursos de archivos

--- bibliotecaModel/recursoBibliotecaM.php
+++ bibliotecaModel/recursoBibliotecaM.php
@@ -4,7 +4,7 @@
 class recursoBibliotecaM extends conexionBD{
     public static function obtenerRecursoM($Etiquetaid){
         try {
-            $pdo = conexionBD::conexion()->prepare("SELECT catrecursos.id, catrecursos.nombre, catrecursos.ruta, catrecursos.titulo, catrecursos.descripcion, catrecursos.autor, catetiquetas.etiquetaDescripcion, catrecursos.tipo, catrecursos.resumen FROM catrecursos INNER JOIN unionetiquetascatrecurso on catrecursos.id = unionetiquetascatrecurso.idRecurso INNER JOIN catetiquetas on unionetiquetascatrecurso.idEtiqueta = catetiquetas.id WHERE unionetiquetascatrecurso.idEtiqueta = :etiqueta");
+            $pdo = conexionBD::conexion()->prepare("SELECT DISTINCT catrecursos.id, catrecursos.nombre, catrecursos.ruta, catrecursos.titulo, catrecursos.descripcion, catrecursos.autor, catetiquetas.etiquetaDescripcion, catrecursos.tipo, catrecursos.resumen FROM catrecursos INNER JOIN unionetiquetascatrecurso on catrecursos.id = unionetiquetascatrecurso.idRecurso INNER JOIN catetiquetas on unionetiquetascatrecurso.idEtiqueta = catetiquetas.id WHERE unionetiquetascatrecurso.idEtiqueta = :etiqueta");
             $pdo->bindParam("etiqueta",$Etiquetaid,PDO::PARAM_INT);
             $pdo->execute();
 
