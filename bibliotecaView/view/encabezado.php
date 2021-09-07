d92d244f3d0b68eb79587782a0061be7255e7ade
TDDTecnologia_Allan <allanmaleman@gmail.com>
2021-09-07 15:16:55 -0600

    Se modificaron las etiquetas META del sitio web

--- bibliotecaView/view/encabezado.php
+++ bibliotecaView/view/encabezado.php
@@ -1,8 +1,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="Description" CONTENT="Author: Tesoros de Dios, La biblioteca virtual es una herramienta que facilita al usuario obtener información acerca de los servicios que como organización brindamos a los niños, jóvenes, y adultos con discapacidad. Dentro de la información puede encontrar estrategias prácticas para ayudar a las personas con discapacidad a obtener un desarrollo integral.">
-    
+    <meta property="og:locale" content="es_ES">
+    <meta property="og:site_name" content="Tesoros de Dios">
+    <meta property="og:title" content="Biblioteca - Tesoros de Dios">
+    <meta property="og:url" content="https://biblioteca.tesorosdedios.org/Ingreso">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="Tesoros de Dios, God’s Treasures, is a non-profit organization that serves children with disabilities in Nicaragua, providing a variety of services to the children and their families.">
     <link rel="Shortcut Icon" type="image/x-icon" href="bibliotecaView/assets/icons/book.ico" />
     <script src="bibliotecaView/js/sweet-alert.min.js"></script>
     <link rel="stylesheet" href="bibliotecaView/css/sweet-alert.css">
@@ -20,7 +24,7 @@
     <script src="bibliotecaView/js/bootstrap.min.js"></script>
     <script src="bibliotecaView/js/jquery.mCustomScrollbar.concat.min.js"></script>
     <script src="bibliotecaView/js/main.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
+    <link   rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
 
 </head>
