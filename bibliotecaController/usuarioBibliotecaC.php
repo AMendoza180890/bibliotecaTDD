2fa0c0337e58e387b6a31fd33992e769c90a0806
AMendoza1890 <allanmaleman@gmail.com>
2024-02-04 18:34:26 -0600

    Se modifico el archivo usuarioBibliotecaC.php para poder iniciar sesion de forma correcta en la biblioteca

--- bibliotecaController/usuarioBibliotecaC.php
+++ bibliotecaController/usuarioBibliotecaC.php
@@ -1,22 +1,23 @@
 <?php
-class usuarioBibliotecaC{
-    public function inciarSesionBibliotecaC(){
+class usuarioBibliotecaC
+{
+    public function inciarSesionBibliotecaC()
+    {
         try {
             if (isset($_POST["usuarioBiblioteca"])) {
-                if(preg_match('/^[a-zA-Z0-9]+$/',$_POST["usuarioBiblioteca"]) && preg_match('/^[a-zA-Z0-9]+$/',$_POST["passwBiblioteca"])){ 
+                if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["usuarioBiblioteca"]) && preg_match('/^[a-zA-Z0-9]+$/', $_POST["passwBiblioteca"])) {
                     $datosUsuario = array("usuario" => $_POST["usuarioBiblioteca"], "passw" => $_POST["passwBiblioteca"]);
-                    $datosUsuarioLogin = usuarioBibliotecaM::iniciarSesionBibliotecaM($datosUsuario); 
+                    $datosUsuarioLogin = usuarioBibliotecaM::iniciarSesionBibliotecaM($datosUsuario);
                     if ($datosUsuarioLogin) {
-                        if ($datosUsuarioLogin["usuario"] == $_POST["usuarioBiblioteca"] && $datosUsuarioLogin["clave"] == $_POST["passwBiblioteca"])
-                        {
-                            if ($datosUsuarioLogin["rolid"] == 1 || $datosUsuarioLogin["rolid"] == 2 ) {
+                        if (password_verify($_POST["passwBiblioteca"], $datosUsuarioLogin["clave"]) && ($datosUsuarioLogin["rolid"] == 1 || $datosUsuarioLogin["rolid"] == 2)) {
+                            if ($datosUsuarioLogin["rolid"] == 1 || $datosUsuarioLogin["rolid"] == 2) {
                                 $_SESSION["ingreso"] = true;
                                 $_SESSION["id"] = $datosUsuarioLogin["id"];
                                 $_SESSION["usuario"] = $datosUsuarioLogin["usuario"];
                                 $_SESSION["clave"] = $datosUsuarioLogin["clave"];
                                 $_SESSION["foto"] = $datosUsuarioLogin["foto"];
                                 $_SESSION["rol"] = $datosUsuarioLogin["catRolesDescripcion"];
-        
+
                                 echo '<script>window.location = "home"</script>';
                             }
                         }
@@ -26,8 +27,7 @@ class usuarioBibliotecaC{
                 }
             }
         } catch (Exception $ex) {
-            echo 'Error - '.$ex;
+            echo 'Error - ' . $ex;
         }
     }
 }
-?>
