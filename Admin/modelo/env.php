6058bc5e925761724783485192552dd399b0effc
AMendoza1890 <allanmaleman@gmail.com>
2024-02-04 18:09:59 -0600

    Se realizo cambios en el GITIGNORE para poder evitar cambios con conexion a la base de datos

--- Admin/modelo/env.php
+++ Admin/modelo/env.php
@@ -1,5 +1,10 @@
 <?php
+// putenv('servidor=localhost');
+// putenv('databaseName=tesorosd_bdbibliotecatdd');
+// putenv('user=tesorosd_tesorosBiblioteca');
+// putenv('passw=Q;hB]p{)Vz(k');
+
 putenv('servidor=localhost');
-putenv('databaseName=tesorosd_bdbibliotecatdd');
-putenv('user=tesorosd_tesorosBiblioteca');
-putenv('passw=Q;hB]p{)Vz(k');
+putenv('databaseName=bibliotecatdd');
+putenv('user=root');
+putenv('passw=');
