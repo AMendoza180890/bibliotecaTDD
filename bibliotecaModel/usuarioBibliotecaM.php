1a26353eca9aa9c6833a78fa9c70ab543d329434
AMendoza1890 <allanmaleman@gmail.com>
2024-02-04 18:17:27 -0600

    Se modifico el archivo usuarioBibliotecaM.php para poder consultar solo por el usuario

--- bibliotecaModel/usuarioBibliotecaM.php
+++ bibliotecaModel/usuarioBibliotecaM.php
@@ -1,22 +1,20 @@
 <?php
-    require_once 'Admin/modelo/conexionBD.php';
-    
-class usuarioBibliotecaM extends conexionBD{
-    public static function iniciarSesionBibliotecaM($datosUsuario){
+require_once 'Admin/modelo/conexionBD.php';
+
+class usuarioBibliotecaM extends conexionBD
+{
+    public static function iniciarSesionBibliotecaM($datosUsuario)
+    {
         try {
-            $pdo = conexionBD::conexion()->prepare("SELECT usuarios.id, usuarios.usuario, usuarios.clave, usuarios.foto, usuarios.rolid, catroles.catRolesDescripcion  FROM usuarios INNER JOIN catroles ON usuarios.rolid = catroles.rolid WHERE usuarios.usuario = :usuario and usuarios.clave = :clave");
+            $pdo = conexionBD::conexion()->prepare("SELECT usuarios.id, usuarios.usuario, usuarios.clave, usuarios.foto, usuarios.rolid, catroles.catRolesDescripcion  FROM usuarios INNER JOIN catroles ON usuarios.rolid = catroles.rolid WHERE usuarios.usuario = :usuario");
+            $pdo->bindParam(":usuario", $datosUsuario["usuario"], PDO::PARAM_STR);
+            $pdo->execute();
 
-            $pdo -> bindParam(":usuario", $datosUsuario["usuario"], PDO::PARAM_STR);
-            $pdo -> bindParam(":clave", $datosUsuario["passw"], PDO::PARAM_STR);
-
-            $pdo -> execute();
-
-            return $pdo -> fetch();
+            return $pdo->fetch();
 
             echo var_dump($pdo->fetch());
         } catch (Exception $ex) {
-            echo 'Error -'.$ex;
+            echo 'Error -' . $ex;
         }
     }
 }
-?>
