9437f534409d4300b7e1eb565adbced2a0875e05
AMendoza1890 <allanmaleman@gmail.com>
2024-02-18 15:19:03 -0600

    Leer la api, con js nativo y ajax

--- Admin/vista/js/recursosVideos.js
+++ Admin/vista/js/recursosVideos.js
@@ -4,24 +4,41 @@ $(".TablaRecursos").on("click", ".EditRegistroRecursoVideo", function() {
 
     datosRecurso.append("idRecurso", codigoRecurso);
 
-    $.ajax({
-        method: "POST",
-        url: "Ajax/recursosVideosA.php",
-        data: datosRecurso,
-        cache: false,
-        contentType: false,
-        processData: false,
-        dataType: "json",
-        success: function(response) {
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "Ajax/recursosVideosA.php", FALSE);
+    xhr.send();
+    xhr.onload = function(response) {
+        if (xhr.status != 200) { // analiza el estado HTTP de la respuesta
+            alert(`Error ${xhr.status}: ${xhr.statusText}`); // ej. 404: No encontrado
+        } else { // muestra el resultado
             $("#idEdit").val(response["id"]);
             $("#titulosEdit").val(response["titulo"]);
             $("#detallesEdit").val(response["nombre"]);
             $("#autorEdit").val(response["autor"]);
             $("#linkEdit").val(response["link"]);
             $("#resumenEditado").val(response["descripcion"]);
-            console.log(response);
-        },
-    });
+        }
+     };
+
+    // $.ajax({
+    //     method: "POST",
+    //     url: "Ajax/recursosVideosA.php",
+    //     data: datosRecurso,
+    //     cache: false,
+    //     contentType: false,
+    //     processData: false,
+    //     dataType: "json",
+    //     success: function(response) {
+    //         $("#idEdit").val(response["id"]);
+    //         $("#titulosEdit").val(response["titulo"]);
+    //         $("#detallesEdit").val(response["nombre"]);
+    //         $("#autorEdit").val(response["autor"]);
+    //         $("#linkEdit").val(response["link"]);
+    //         $("#resumenEditado").val(response["descripcion"]);
+    //         console.log(response);
+    //     },
+    // });
 
 
     $.ajax({
--- Admin/vista/js/usuario.js
+++ Admin/vista/js/usuario.js
@@ -2,20 +2,17 @@
 // funcion para obtener registros por ajax
 $(".TablaUsuario").on("click", ".EditRegistroUsuario", function() {
     let codValor = $(this).attr("codValor");
-
     let datos = new FormData();
-
     datos.append("id", codValor);
 
-    $.ajax({
-        method: "POST",
-        url: "Ajax/usuarioA.php",
-        data: datos,
-        cache: false,
-        contentType: false,
-        processData: false,
-        dataType: "json",
-        success: function (response) {
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "Ajax/usuarioA.php", FALSE);
+    xhr.send();
+    xhr.onload = function(response) {
+        if (xhr.status != 200) { // analiza el estado HTTP de la respuesta
+            alert(`Error ${xhr.status}: ${xhr.statusText}`); // ej. 404: No encontrado
+        } else { // muestra el resultado
             $("#idEdit").val(response["id"]);
             $("#usuarioEdit").val(response["usuario"]);
             $("#claveEdit").val(response["clave"]);
@@ -33,7 +30,36 @@ $(".TablaUsuario").on("click", ".EditRegistroUsuario", function() {
                 $(".visor").attr("src","vista/img/usuario/defecto.png")
             }
         }
-    });
+     };
+
+
+    // $.ajax({
+    //     method: "POST",
+    //     url: "Ajax/usuarioA.php",
+    //     data: datos,
+    //     cache: false,
+    //     contentType: false,
+    //     processData: false,
+    //     dataType: "json",
+    //     success: function (response) {
+    //         $("#idEdit").val(response["id"]);
+    //         $("#usuarioEdit").val(response["usuario"]);
+    //         $("#claveEdit").val(response["clave"]);
+    //         $("#emailEdit").val(response["email"]);
+    //         $("#organizacionEdit").val(response["organizacion"]);
+    //         $("#cargoEdit").val(response["cargo"]);
+    //         $("#telefonoEdit").val(response["telefono"]);
+    //         $("#rolEdit").html(response["catRolesDescripcion"]);
+    //         $("#rolEdit").val(response["rolid"]);
+    //         $("#fotoActual").val(response["foto"]);
+
+    //         if (response["foto"] != "") {
+    //             $(".visor").attr("src",response["foto"]);
+    //         }else{
+    //             $(".visor").attr("src","vista/img/usuario/defecto.png")
+    //         }
+    //     }
+    // });
 })
 
 // funcion para desactivar registro por GET
