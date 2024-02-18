7f697154a7782dba5e124d2f264a03ddbfabd943
AMendoza1890 <allanmaleman@gmail.com>
2024-02-18 16:11:57 -0600

    Se agrega .env en .gitignore, se esta incluyendo js nativo en las llamadas de las api

--- Admin/vista/js/recursosVideos.js
+++ Admin/vista/js/recursosVideos.js
@@ -4,10 +4,9 @@ $(".TablaRecursos").on("click", ".EditRegistroRecursoVideo", function() {
 
     datosRecurso.append("idRecurso", codigoRecurso);
 
-
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "Ajax/recursosVideosA.php", FALSE);
-    xhr.send();
+    xhr.send(datosRecurso);
     xhr.onload = function(response) {
         if (xhr.status != 200) { // analiza el estado HTTP de la respuesta
             alert(`Error ${xhr.status}: ${xhr.statusText}`); // ej. 404: No encontrado
@@ -40,21 +39,54 @@ $(".TablaRecursos").on("click", ".EditRegistroRecursoVideo", function() {
     //     },
     // });
 
-
-    $.ajax({
-        method: "POST",
-        url: "Ajax/EtiquetaVideoA.php",
-        data: datosRecurso,
-        cache: false,
-        contentType: false,
-        processData: false,
-        dataType: "json",
-        success: function(response) {
-            console.log(response);
-            for (let i = 0; i <= Object.keys(response).length; i++) {
+    xhr.open("POST", "Ajax/EtiquetaVideoA.php", FALSE);
+    xhr.send(datosRecurso);
+    xhr.onload = function(response) {
+        if (xhr.status != 200) { // analiza el estado HTTP de la respuesta
+            alert(`Error ${xhr.status}: ${xhr.statusText}`); // ej. 404: No encontrado
+        } else { // muestra el resultado
+           for (let i = 0; i <= Object.keys(response).length; i++) {
                 $("#eleccion" + i).val(response[i]["etiquetaId"]);
                 $("#eleccion" + i).html(response[i]["etiquetaDescripcion"]);
             }
-        },
-    });
+        }
+     };
+    
+    //  $.ajax({
+    //     method: "POST",
+    //     url: "Ajax/EtiquetaVideoA.php",
+    //     data: datosRecurso,
+    //     cache: false,
+    //     contentType: false,
+    //     processData: false,
+    //     dataType: "json",
+    //     success: function(response) {
+    //         console.log(response);
+    //         for (let i = 0; i <= Object.keys(response).length; i++) {
+    //             $("#eleccion" + i).val(response[i]["etiquetaId"]);
+    //             $("#eleccion" + i).html(response[i]["etiquetaDescripcion"]);
+    //         }
+    //     },
+    // });
 });
+
+
+const documentoLink = document.getElementById("linkNuevo");
+documentoLink.onchange = function(){
+    const options = {
+        method: 'POST',
+        headers: {
+            accept: 'application/json',
+            'x-apikey': '231c1810edd3c20173029964fb79ab84c2c0a37cd03495a6ff6122bef36fab58',
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            url: documentoLink
+        })
+        };
+
+        fetch('https://www.virustotal.com/api/v3/urls', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+}
