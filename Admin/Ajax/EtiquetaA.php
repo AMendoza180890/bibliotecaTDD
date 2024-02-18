5593ef6fa9e188f85822661fd4db6dc7cee56a20
AMendoza1890 <allanmaleman@gmail.com>
2024-02-18 14:10:49 -0600

    Se corrigio la ruta en los archivos en la carpeta ajax

--- Admin/Ajax/EtiquetaA.php
+++ Admin/Ajax/EtiquetaA.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../vendor/autoload.php';
+require_once '../../vendor/autoload.php';
 include_once '../controlador/recursosC.php';
 include_once '../modelo/recursosM.php';
 
--- Admin/Ajax/recursosVideosA.php
+++ Admin/Ajax/recursosVideosA.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../vendor/autoload.php';
+require_once '../../vendor/autoload.php';
 include_once '../controlador/recursosVideosC.php';
 include_once '../modelo/recursosVideosM.php';
 
